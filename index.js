0a4d6ce4e7983dad16428f3981e055f1e4cc1341
Por Levente <porleventeferenc@gmail.com>
2026-03-31 14:51:21 +0200

    table formatting todo

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const promise = fetch("https://api.siposm.hu/getDevelopers")
 promise.then(response => response.json().then(table_data =>{
 
-}
-));
-
-const table_element = document.createElement("table")
-const table_target = document.querySelector("#table-target")
-
-table_target.appendChild(table_element)
-
-const table_head = document.createElement("thead")
-table_element.appendChild(table_head)
-
-for (const developer of developers) {
-    const table_row_element = document.createElement("tr")
-    for (const property in developer) {
-        if (!Object.hasOwn(developer, property)) continue;
-        
-        const property_value = developer[property];
-        
-        if(property === )
+    for (const developer of table_data) {
+        const table_row_element = document.createElement("tr")
+        for (const property in developer) {
+            if (!Object.hasOwn(developer, property)) continue;
+            const property_value = developer[property];
+            const table_divider_element = document.createElement("td")
+            
+            if (property === "name") {
+                table_divider_element.append(property_value)
+            }
+            if(property === "email"){
+                table_divider_element.append(property_value)
+            }
+            if (property === "age") {
+                table_divider_element.append(property_value)
+            }
+            if (property === "salary") {
+                table_divider_element.append(property_value)
+            }
+            table_row_element.appendChild(table_divider_element)
+        }
+        const table_element = document.querySelector("table")
+        table_element.appendChild(table_row_element)
     }
 }
+));
